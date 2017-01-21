9ea1daf2d68028dad440778abb7670d1ab74ef8d
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-21 16:15:43 +0100

    chore(build): added rollup config file

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import buble from 'rollup-plugin-buble'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+  entry: 'src/index.js',
+  dest: 'dist/index.js',
+  format: 'cjs',
+  moduleName: 'jscad-scad-api',
+  sourceMap: true,
+  external: [
+    'csg'
+  ],
+  plugins: [
+    buble(),
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      namedExports: {
+        'node_modules/csg/csg.js': [ 'CSG', 'CAG' ]
+      }
+    })
+  ]
+}
