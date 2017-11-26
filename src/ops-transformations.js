fde94a383f041df7475220ff2b88342bc91263b0
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-11-26 12:51:25 +0100

    refactor(ops): further docstring additions & refactorings

--- src/ops-transformations.js
+++ src/ops-transformations.js
@@ -173,9 +173,8 @@ function minkowski () {
 }
 
 /** create a convex hull of the given shapes
- * @param {[Array]} vector - the axes to mirror the object(s) by
  * @param {[Object]} objects either a single or multiple CSG/CAG objects to create a hull around
- * @returns {CSG} new CSG object , mirrored
+ * @returns {CSG} new CSG object , a hull around the given shapes
  *
  * @example
  * let hulled = hull(rect(), circle())
